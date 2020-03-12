11e0d6d1231563dc49c99cf6a8ea2454bce400b9
faladetimilehin <faladetimilehin@yahoo.com>
2020-03-12 16:25:16 +0100

    login page and register page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Contact from './components/Contact';
+import About from './components/About';
+import Register from './components/Register'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
+class App extends Component{
+  render(){
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Navbar/>
+        <Route exact path='/Home' component={Home}/>
+        <Route path='/About' component={About}/>
+        <Route path='/Contact' component={Contact}/>
+        <Route path='/Login' component={Login}/>
+        <Route path='/Register' component={Register}/>
+      </div>
+      </BrowserRouter>
+    );
+  }
+  
+  }
+ 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>ddd
+        </div>
+    )
+}
+export default Contact;
