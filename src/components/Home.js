7a9a782f61c2226c4a1018540f4601fd56edb172
faladetimilehin <faladetimilehin@yahoo.com>
2020-03-16 16:45:05 +0100

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -112,7 +112,7 @@ const Home = () => {
                     <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
                 </div>
                 <div class="col-md-5 order-md-1">
-                    <img class="featurette-image img-fluid mx-auto" src={img4} alt="Generic placeholder "></img>
+                    <img class="featurette-image img-fluid mx-auto" src={img4 } alt="Generic placeholder "></img>
                 </div>
             </div>
 
